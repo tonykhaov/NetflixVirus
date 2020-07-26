c066c758110bdc74fdcc872adf804d30770d5f4a
Tony Khaov <tony.khaov@gmail.com>
2020-07-26 14:57:39 +0200

    :lipstick: Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
+import styles from "../styles/footer.module.scss";
+
 const Footer = () => {
   return (
-    <footer>
-      <h1>hello</h1>
+    <footer className={styles.footer}>
+      <p className={styles["social-message"]}>We're also on social medias</p>
+      <div className={styles["social-medias"]}>
+        <a href="http://twitter.com/netflix">Twitter</a>
+        <a href="http://instagram.com/netflix">Instagram</a>
+        <a href="http://facebook.com/netflix">Facebook</a>
+      </div>
     </footer>
   );
 };
