2a3340fbe15c5188001b8c24186d82b72d3a0265
Tony Khaov <tony.khaov@gmail.com>
2020-07-27 19:02:24 +0200

    :sparkles: Try getServerSideProps()

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-const MoviesList = () => {
+const MoviesList = ({ props }) => {
+  console.log(props);
   return (
     <MoviesListWrapper>
       <h1>MoviesList</h1>
@@ -19,4 +20,18 @@ const MoviesListWrapper = styled.main`
     padding: 12px 8px;
   }
 `;
+
+export async function getServerSideProps() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=eb1340ed9b9e6d049cad9a073a4a431b&language=en-EN&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1"
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
 export default MoviesList;
