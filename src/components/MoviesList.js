ebf50e1b7eaa7aaefe95ceaad765ce6ab4fbc446
Tony Khaov <tony.khaov@gmail.com>
2020-07-27 18:20:05 +0200

    :lipstick: Add styles to <MoviesList/>

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,9 +1,19 @@
+import styled from "styled-components";
+
 const MoviesList = () => {
   return (
-    <div>
+    <MoviesListWrapper>
       <h1>MoviesList</h1>
-    </div>
+    </MoviesListWrapper>
   );
 };
 
+const MoviesListWrapper = styled.main`
+  min-height: 90vh;
+  padding: 24px 32px;
+
+  @media (max-width: 640px) {
+    padding: 12px 8px;
+  }
+`;
 export default MoviesList;
