01346e0b9c8ead6a248fbe60d8bfb00db395ba8f
Tony Khaov <tony.khaov@gmail.com>
2020-07-27 19:37:31 +0200

    :sparkles: Add getStaticProps to fetch movies list

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import MoviesList from "../components/MoviesList";
 import Footer from "../components/Footer";
 
-export default function Home() {
+function Home({ movies }) {
   return (
     <div>
       <Head>
@@ -15,8 +15,23 @@ export default function Home() {
         />
       </Head>
       <Header />
-      <MoviesList />
+      <MoviesList movies={movies} />
       <Footer />
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?api_key=eb1340ed9b9e6d049cad9a073a4a431b&language=en-EN&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1"
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      movies: data.results,
+    },
+  };
+}
+
+export default Home;
