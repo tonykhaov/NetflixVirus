dde18acb8e7751529217b04e5001e67f7fca3215
Tony Khaov <tony.khaov@gmail.com>
2020-07-27 17:30:13 +0200

    :lipstick: <Footer/> Move module scss into styled-components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,44 @@
-import styles from "../styles/footer.module.scss";
+import styled from "styled-components";
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <p className={styles["social-message"]}>We're also on social medias</p>
-      <div className={styles["social-medias"]}>
+    <FooterWrapper>
+      <p className="social-message">We're also on social medias</p>
+      <div className="social-medias">
         <a href="http://twitter.com/netflix">Twitter</a>
         <a href="http://instagram.com/netflix">Instagram</a>
         <a href="http://facebook.com/netflix">Facebook</a>
       </div>
-    </footer>
+    </FooterWrapper>
   );
 };
+
+const FooterWrapper = styled.footer`
+  background-color: #222;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0.6rem 0 0.8rem;
+
+  .social-message {
+    color: grey;
+  }
+
+  .social-medias {
+    display: flex;
+    justify-content: space-around;
+    background: red;
+    align-items: center;
+    width: 100%;
+    max-width: 720px;
+  }
+
+  @media (min-width: 720px) {
+    .social-medias {
+      width: 720px;
+    }
+  }
+`;
 
 export default Footer;
