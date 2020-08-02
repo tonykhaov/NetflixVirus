84ce0cc50d9e857cf815cc644def5a6aa1b744e3
Tony Khaov <tony.khaov@gmail.com>
2020-08-02 14:32:32 +0200

    :construction_worker: Update test to test href links

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -1,7 +1,20 @@
 import Footer from "../Footer";
 import { render } from "@testing-library/react";
+import { toHaveAttribute } from "@testing-library/jest-dom";
 
 test("<Footer/>", () => {
   const { getByTestId } = render(<Footer />);
-  expect(getByTestId("social-message").textContent).toBe("We're also on social medias");
+  expect(getByTestId("social-message")).toHaveTextContent("We're also on social medias");
+  expect(getByTestId("social-media--link__twitter")).toHaveAttribute(
+    "href",
+    "https://twitter.com/netflix"
+  );
+  expect(getByTestId("social-media--link__instagram")).toHaveAttribute(
+    "href",
+    "https://instagram.com/netflix"
+  );
+  expect(getByTestId("social-media--link__facebook")).toHaveAttribute(
+    "href",
+    "https://facebook.com/netflix"
+  );
 });
