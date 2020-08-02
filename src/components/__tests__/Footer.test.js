628ce0337726376fb00c020c69570d7580177e72
Tony Khaov <tony.khaov@gmail.com>
2020-08-02 14:55:17 +0200

    :construction_worker: Split the tests inside a describe

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -2,26 +2,34 @@ import Footer from "../Footer";
 import { render } from "@testing-library/react";
 import { toHaveAttribute } from "@testing-library/jest-dom";
 
-test("<Footer/>", () => {
-  const { getByTestId } = render(<Footer />);
+describe("<Footer/>", function () {
+  test("Message is We're also on social medias?", () => {
+    const { getByTestId } = render(<Footer />);
+    expect(getByTestId("social-message")).toHaveTextContent("We're also on social medias");
+  });
 
-  expect(getByTestId("social-message")).toHaveTextContent("We're also on social medias");
+  test("Icons in the document?", () => {
+    const { getByTestId } = render(<Footer />);
+    expect(getByTestId("instagram-icon")).toBeInTheDocument();
+    expect(getByTestId("twitter-icon")).toBeInTheDocument();
+    expect(getByTestId("facebook-icon")).toBeInTheDocument();
+  });
 
-  expect(getByTestId("twitter-icon")).toBeInTheDocument();
-  expect(getByTestId("social-media--link__twitter")).toHaveAttribute(
-    "href",
-    "https://twitter.com/netflix"
-  );
+  test("Links have valid href?", () => {
+    const { getByTestId } = render(<Footer />);
+    expect(getByTestId("social-media--link__twitter")).toHaveAttribute(
+      "href",
+      "https://twitter.com/netflix"
+    );
 
-  expect(getByTestId("instagram-icon")).toBeInTheDocument();
-  expect(getByTestId("social-media--link__instagram")).toHaveAttribute(
-    "href",
-    "https://instagram.com/netflix"
-  );
+    expect(getByTestId("social-media--link__instagram")).toHaveAttribute(
+      "href",
+      "https://instagram.com/netflix"
+    );
 
-  expect(getByTestId("facebook-icon")).toBeInTheDocument();
-  expect(getByTestId("social-media--link__facebook")).toHaveAttribute(
-    "href",
-    "https://facebook.com/netflix"
-  );
+    expect(getByTestId("social-media--link__facebook")).toHaveAttribute(
+      "href",
+      "https://facebook.com/netflix"
+    );
+  });
 });
