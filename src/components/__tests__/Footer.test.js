7eef34044152aca2bf25aff2abfe8f7b761112bb
Tony Khaov <tony.khaov@gmail.com>
2020-08-02 15:07:11 +0200

    :construction_worker: Update test using screen API

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -1,33 +1,34 @@
 import Footer from "../Footer";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { toHaveAttribute } from "@testing-library/jest-dom";
 
 describe("<Footer/>", function () {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+
   test("Message is We're also on social medias?", () => {
-    const { getByTestId } = render(<Footer />);
-    expect(getByTestId("social-message")).toHaveTextContent("We're also on social medias");
+    expect(screen.getByTestId("social-message")).toHaveTextContent("We're also on social medias");
   });
 
   test("Icons in the document?", () => {
-    const { getByTestId } = render(<Footer />);
-    expect(getByTestId("instagram-icon")).toBeInTheDocument();
-    expect(getByTestId("twitter-icon")).toBeInTheDocument();
-    expect(getByTestId("facebook-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("instagram-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("twitter-icon")).toBeInTheDocument();
+    expect(screen.getByTestId("facebook-icon")).toBeInTheDocument();
   });
 
   test("Links have valid href?", () => {
-    const { getByTestId } = render(<Footer />);
-    expect(getByTestId("social-media--link__twitter")).toHaveAttribute(
+    expect(screen.getByTestId("social-media--link__twitter")).toHaveAttribute(
       "href",
       "https://twitter.com/netflix"
     );
 
-    expect(getByTestId("social-media--link__instagram")).toHaveAttribute(
+    expect(screen.getByTestId("social-media--link__instagram")).toHaveAttribute(
       "href",
       "https://instagram.com/netflix"
     );
 
-    expect(getByTestId("social-media--link__facebook")).toHaveAttribute(
+    expect(screen.getByTestId("social-media--link__facebook")).toHaveAttribute(
       "href",
       "https://facebook.com/netflix"
     );
