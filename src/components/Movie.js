736c590c8e45f23a7c10d40cb109605c2e79ab16
Tony Khaov <tony.khaov@gmail.com>
2020-08-03 19:26:39 +0200

    :lipstick: Style Image Cover

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,19 +5,19 @@ const IMG_PATH = "http://image.tmdb.org/t/p/w185/";
 
 function Movie({ movie }) {
   return (
-    <Link href={`/movie/${movie.id}`}>
-      <Img src={`${IMG_PATH}${movie.poster_path}`} alt={movie.title} />
+    <Link href={`/movie/${movie.id}`} as={`/movie/${movie.id}`}>
+      <ImgStyled src={`${IMG_PATH}${movie.poster_path}`} alt={movie.title} />
     </Link>
   );
 }
 
-export const Img = styled.img`
-  box-shadow: 0 0 12px black;
+export const ImgStyled = styled.img`
+  box-shadow: 0 0 6px black;
   cursor: pointer;
+  transition: box-shadow 0.1s linear;
   &:hover,
   &:focus {
     box-shadow: 0 0 3px black;
-    transition: transform 0.1s linear;
   }
 `;
 
