a385497383ba9b0142d5c83079436f124149ccfd
Tony Khaov <tony.khaov@gmail.com>
2020-08-03 18:37:57 +0200

    :construction: Add Header and Footer to <MovieDetails/>

--- src/pages/movie/[id].js
+++ src/pages/movie/[id].js
@@ -1,9 +1,14 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 const MovieDetails = () => {
   return (
     <>
+      <Header />
       <div>
         <h1>Hello</h1>
       </div>
+      <Footer />
     </>
   );
 };
