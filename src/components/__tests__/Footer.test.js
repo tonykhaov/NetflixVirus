3591ef87478da0793db378348d625ad7cc99efa2
Tony Khaov <tony.khaov@gmail.com>
2020-08-04 16:14:19 +0200

    :construction_worker: Update test and delete beforeEach

--- src/components/__tests__/Footer.test.js
+++ src/components/__tests__/Footer.test.js
@@ -3,21 +3,20 @@ import { render, screen } from "@testing-library/react";
 import { toHaveAttribute } from "@testing-library/jest-dom";
 
 describe("<Footer/>", function () {
-  beforeEach(() => {
-    render(<Footer />);
-  });
-
   test("Message is We're also on social medias?", () => {
+    render(<Footer />);
     expect(screen.getByText("We're also on social medias")).toBeInTheDocument();
   });
 
   test("Icons in the document?", () => {
+    render(<Footer />);
     expect(screen.getByTestId("instagram-icon")).toBeInTheDocument();
     expect(screen.getByTestId("twitter-icon")).toBeInTheDocument();
     expect(screen.getByTestId("facebook-icon")).toBeInTheDocument();
   });
 
   test("Links have valid href?", () => {
+    render(<Footer />);
     expect(screen.getByTestId("social-media--link__twitter")).toHaveAttribute(
       "href",
       "https://twitter.com/netflix"
