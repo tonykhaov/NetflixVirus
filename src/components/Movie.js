b7198d666dfae47996ba75d24bff3cbe5bd0173b
Tony Khaov <tony.khaov@gmail.com>
2020-08-04 22:49:35 +0200

    :lipstick: Change transform translate to scale

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -16,10 +16,10 @@ const IMG_PATH = "http://image.tmdb.org/t/p/w185/";
 export const ImgStyled = styled.img`
   box-shadow: 0 0 3px black;
   cursor: pointer;
-  transition: transform 0.4s cubic-bezier(0.13, 0.47, 0.61, 0.88);
+  transition: transform 0.2s cubic-bezier(0.13, 0.47, 0.61, 0.88);
   &:hover,
   &:focus {
-    transform: translate(-2px, -6px);
+    transform: scale(1.05);
   }
 `;
 
