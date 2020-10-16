e72ffbb732fb15bd1539c8ba300db4da2862da53
Tony Khaov <tony.khaov@gmail.com>
2020-10-16 21:43:41 +0200

    Make getMoviePages instead of dupicating fetches

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,22 +1,21 @@
 export async function getMovies() {
-  const res = await Promise.all([
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=eb1340ed9b9e6d049cad9a073a4a431b&language=en-EN&sort_by=vote_count.desc&include_adult=false&include_video=false&page=1"
-    ),
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=eb1340ed9b9e6d049cad9a073a4a431b&language=en-EN&sort_by=vote_count.desc&include_adult=false&include_video=false&page=2"
-    ),
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=eb1340ed9b9e6d049cad9a073a4a431b&language=en-EN&sort_by=vote_count.desc&include_adult=false&include_video=false&page=3"
-    ),
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=eb1340ed9b9e6d049cad9a073a4a431b&language=en-EN&sort_by=vote_count.desc&include_adult=false&include_video=false&page=4"
-    ),
-  ]);
-
+  const moviePages = getMoviePages();
+  const res = await Promise.all(moviePages);
   const data = await Promise.all(res.map((response) => response.json()));
-
   const movies = data.flatMap((movie) => movie.results);
 
   return movies;
 }
+
+function getMoviePages(limit = 6) {
+  if (limit < 1) limit = 2;
+  const moviePages = [];
+  for (let i = 1; i < limit; i++) {
+    moviePages.push(fetch(fetchingUrl + i));
+  }
+
+  return moviePages;
+}
+
+const fetchingUrl =
+  "https://api.themoviedb.org/3/discover/movie?api_key=eb1340ed9b9e6d049cad9a073a4a431b&language=en-EN&sort_by=vote_count.desc&include_adult=false&include_video=false&page=";
