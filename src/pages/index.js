76e9c667e1119714a4c7d1dc3707a2fb2e9cf08d
Tony Khaov <tony.khaov@gmail.com>
2020-10-17 09:50:56 +0200

    Remove MoviesList and move it to index.js directly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
-import MoviesList from "../components/MoviesList";
+import styled from "styled-components";
+import Movie from "../components/Movie";
 import { getMovies } from "../utils/utils";
 
 function Home({ movies }) {
   return (
-    <>
-      <MoviesList movies={movies} />
-    </>
+    <MoviesListWrapper>
+      {movies.map((movie) => (
+        <Movie movie={movie} key={movie.id} />
+      ))}
+    </MoviesListWrapper>
   );
 }
 
@@ -18,5 +21,18 @@ export async function getStaticProps() {
     },
   };
 }
+
+const MoviesListWrapper = styled.main`
+  min-height: 800px;
+  padding: 24px 32px;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(185px, 1fr));
+  justify-items: center;
+  grid-gap: 16px 12px;
+  @media (max-width: 640px) {
+    padding: 12px 8px;
+  }
+`;
 
 export default Home;
