75004716d4540c91a0a49c86731c297b4f85fc32
Tony Khaov <tony.khaov@gmail.com>
2021-01-13 20:22:58 +0100

    update pizza duo reviews

--- src/pages/api/pizzaduo/[maxPost].js
+++ src/pages/api/pizzaduo/[maxPost].js
@@ -1,34 +1,50 @@
-import Cors from "cors";
-import pizzaduoReviews from "../../../db/pizzaduoReview";
+import Cors from 'cors'
+import pizzaduoReviews from '../../../db/pizzaduoReview'
 
 const cors = Cors({
-  methods: ["GET", "HEAD"],
-});
+  methods: ['GET', 'HEAD'],
+})
 
 async function runMiddleware(req, res, fn) {
   return new Promise((resolve, reject) => {
     fn(req, res, (result) => {
       if (result instanceof Error) {
-        return reject(result);
+        return reject(result)
       }
 
-      return resolve(result);
-    });
-  });
+      return resolve(result)
+    })
+  })
 }
 
+// SCRIPT TO GET REVIEWS
+//
+// const divReviews = [...document.querySelectorAll('.jxjCjc')]
+//
+// JSON.stringify(
+//   divReviews.map((element) => {
+//     const customer = {
+//       name: element.querySelector('a[href]').textContent,
+//       comment: element.querySelector('span[jscontroller=P7L8k]').textContent,
+//       review: element.querySelector('.Fam1ne').getAttribute('aria-label'),
+//       date: element.querySelector('.dehysf').textContent,
+//       photoUrl: element.parentNode.querySelector('img').src,
+//     }
+//     return customer
+//   }),
+// )
+
 export default async (req, res) => {
-  await runMiddleware(req, res, cors);
-  const { maxPost } = req.query;
-  const numberedMaxPost = Number(maxPost);
+  await runMiddleware(req, res, cors)
+  const { maxPost } = req.query
+  const numberedMaxPost = Number(maxPost)
 
   if (isNaN(numberedMaxPost) || numberedMaxPost > 118) {
     res.json({
-      error:
-        "Invalid maxPost query either it is greater than 118 or it is not a number",
-    });
+      error: 'Invalid maxPost query either it is greater than 118 or it is not a number',
+    })
   }
 
-  const slicedReviews = pizzaduoReviews.slice(0, numberedMaxPost);
-  res.json(slicedReviews);
-};
+  const slicedReviews = pizzaduoReviews.slice(0, numberedMaxPost)
+  res.json(slicedReviews)
+}
